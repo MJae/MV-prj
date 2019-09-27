9a3d31d29c0e8deeb5e062b77fe82907b587219c
Joan Advincula <jadvincula.cloud@protonmail.com>
2019-09-27 18:09:00 +0800

    Complete new version of Rock, Paper, Scissors

--- the_odin_project/rock_paper_scissors-gui/rps-game-v-gui.js
+++ the_odin_project/rock_paper_scissors-gui/rps-game-v-gui.js
@@ -13,26 +13,73 @@ function computerPlay() {
 function playRound() {
     const computerSelection = computerPlay();
     const playerSelection = this.id;
-    console.log(playRound);
+
+    const cScore = document.getElementById("computerScore");
+    const pScore = document.getElementById("playerScore");
+
+    // Display computer hand
+    const cHand = document.getElementById("computerHand");
+    const cHandFile = "images/rps-" + computerSelection + ".png";
+    cHand.src = cHandFile;
+
+    if (playerSelection == computerSelection) {
+        playerScore++;
+        computerScore++;
+
+        cScore.textContent = computerScore;
+        pScore.textContent = playerScore;
+    }
+    
+    if (playerSelection == "rock" && computerSelection == "paper" ||
+        playerSelection == "paper" && computerSelection == "scissors" ||
+        playerSelection == "scissors" && computerSelection == "rock") {
+        // Computer wins
+        computerScore++;
+        cScore.textContent = computerScore;
+    } else {
+        // Player wins
+        playerScore++;
+        pScore.textContent = playerScore;
+    }
+
+    // Declare winner if either player has already reached five points
+    let gameWinner = "none";
+    if (playerScore >= 5) {
+        if (computerScore < 5) {
+            gameWinner = "Player";
+        } else if (computerScore >= 5) {
+            gameWinner = "Tie"
+        }
+    } else if (computerScore >= 5) {
+        gameWinner = "Computer";
+    }
+
+    if (gameWinner != "none") {
+        const winnerDisplay = document.getElementById("winnerName");
+        winnerDisplay.textContent = gameWinner;
+
+        // Reset scores if someone has already won
+        playerScore = 0;
+        computerScore = 0;
+        cScore.textContent = computerScore;
+        pScore.textContent = playerScore;
+    }
+
+    return;
 }
 
 function playGame() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let numRounds = 0;
-    let maxRounds = 5;
+    const buttonRock = document.getElementById("rock");
+    const buttonPaper = document.getElementById("paper");
+    const buttonScissors = document.getElementById("scissors");
 
-    while (numRounds < maxRounds) {
-        const buttonRock = document.getElementById("rock");
-        const buttonPaper = document.getElementById("paper");
-        const buttonScissors = document.getElementById("scissors");
+    buttonRock.addEventListener("click", playRound);
+    buttonPaper.addEventListener("click", playRound);
+    buttonScissors.addEventListener("click", playRound);
 
-        buttonRock.addEventListener("click", playRound);
-        buttonPaper.addEventListener("click", playRound);
-        buttonScissors.addEventListener("click", playRound);
-
-        numRounds++;
-    }
 }
+
+let playerScore = 0;
+let computerScore = 0;
 
 window.addEventListener("load", playGame);
